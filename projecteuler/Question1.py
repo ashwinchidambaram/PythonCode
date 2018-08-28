ae9e904e9135710f8967c8ada7682b339f1d9934
Ashwin Chidambaram <ashwinchidambaram@eduroam-169-233-222-196.ucsc.edu>
2018-08-28 16:06:48 -0700

    Finished Question 1

---
+++ projecteuler/Question1.py
@@ -0,0 +1,25 @@
+# Ashwin Chidambaram 											##
+# Task: Find the sum of all the multiples of 3 or 5 below 1000	##
+##################################################################
+
+# Declare sum and number variables
+num = 0
+sumValue = 0
+
+# Use while loop to see if we have iterated up to 1000
+while num != 1000:
+	
+	# Check if num is divisible by 3 or 5
+	if (num % 3 == 0) or (num % 5 == 0):
+		# Sum value 
+		sumValue += num
+
+		# Increment value of num
+		num += 1
+
+	else:
+		# Increment value of num
+		num += 1
+
+# Print sum of multiples of 3 or 5 less than 1000
+print(sumValue)
