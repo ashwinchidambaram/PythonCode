00abef56293586441dcf8fa77bd03bb65f4c444e
Ashwin Chidambaram <ashwinchidambaram@eduroam-169-233-222-196.ucsc.edu>
2018-08-29 14:57:14 -0700

    Solved Question 5

--- projecteuler/Question5.py
+++ projecteuler/Question5.py
@@ -1,3 +1,49 @@
 # Ashwin Chidambaram                                                                                        ##
 # Task: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?   ##
 ##############################################################################################################
+# Self assesment on program runtime ##
+import timeit                       ##
+start = timeit.default_timer()      ##
+######################################
+
+# Set base variables to be used
+check = None    # Used to see if the number is divisible
+a = 0           # Divisor that will be changing
+
+# Create a while loop that will check if the number is divisible from 1 - 20
+while check != True:
+    # Declare base value of dividen value
+    num = 1         # Dividend value that will go 1-20
+    a = a + 1       # Increment a by a value of 1
+
+    # Set divisible to True as a method to make a loop that will work more efficently
+    check = True
+
+    # Create a while loop that will loop until num = 20 or divisble becomes False
+    while check == True:
+
+        # Divide value by num
+        quo = a % num
+
+        # Check if value evendly divides
+        if quo != 0:
+            check = False
+
+            # If false, break and go to next number
+            break
+
+        # Increment number by 1 to get next divisor
+        num = num + 1
+
+        # If the condition where the numbers eventually reach 21, that means we've exceed our limit of 20 and break there
+        if num == 21:
+            break
+
+# End timer
+stop = timeit.default_timer()
+
+# Print output
+print("Smallest positive number evenly divisible by 1 to 20: {}".format(a))
+
+# Print runtime
+print('RunTime: {} seconds'.format(round((stop - start),4)))
