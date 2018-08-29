d062d7b93229e7bc4155bb91de27070d3699a207
Ashwin Chidambaram <ashwinchidambaram@eduroam-169-233-222-196.ucsc.edu>
2018-08-29 16:52:55 -0700

    Created Question 6.py

---
+++ projecteuler/Question6.py
@@ -0,0 +1,22 @@
+# Ashwin Chidambaram                                                                                                            ##
+# Task: Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum   ##
+##################################################################################################################################
+# Self assesment on program runtime ##
+import time                         ##
+start_time = time.time()            ##
+######################################
+
+
+
+
+
+
+
+
+# End runtime measure
+runtime = time.time() - start_time
+
+# Print output
+
+# Print runtime
+print('RunTime: {} seconds'.format(round(runtime,4)))
