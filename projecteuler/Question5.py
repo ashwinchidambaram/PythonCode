3241ceac92d0aa150a183aeb1c5cbdc0ba128238
Ashwin Chidambaram <ashwinchidambaram@eduroam-169-233-222-196.ucsc.edu>
2018-08-29 13:38:26 -0700

    Created Question 5

---
+++ projecteuler/Question5.py
@@ -0,0 +1,3 @@
+# Ashwin Chidambaram                                                                                        ##
+# Task: What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?   ##
+##############################################################################################################
