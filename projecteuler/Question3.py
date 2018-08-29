271ab54ea7195e3ad2ee408d68f8b64e631ed993
Ashwin Chidambaram <ashwinchidambaram@eduroam-169-233-222-196.ucsc.edu>
2018-08-28 18:21:27 -0700

    Program gets all factors of a number

--- projecteuler/Question3.py
+++ projecteuler/Question3.py
@@ -1,3 +1,25 @@
 # Ashwin Chidambaram                                                ##
-# Task: What is the largest prime factor of the number 600851475143 ##	                                        
+# Task: What is the largest prime factor of the number 600851475143 ##
 ######################################################################
+import math
+
+# Create a list to hold the factors of 600851475143
+factorList = []
+
+# Create a variable to store the number that will be divided into 600851475143 to see if it's a factor
+num = 1
+
+# Create a while loop to iterate till it reaches half the value of the original number
+while num <= (math.sqrt(13195)):
+
+    # Take the mod of 600851475143 % num and check if = 0. If true, then its a factor. If false, not a factor
+    if 13195 % num == 0:
+
+        # Add number to list
+        factorList.append(num)
+        num += 1
+
+    else:
+        num += 1
+
+print(factorList)#############
