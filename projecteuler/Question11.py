f1618c93bfab799179265c6c1af39162f89a6451
Ashwin Chidambaram <ashwinchidambaram@Ashwins-MacBook-Pro.local>
2018-09-07 01:23:15 -0700

    Created Question 11

---
+++ projecteuler/Question11.py
@@ -0,0 +1,20 @@
+# Ashwin Chidambaram                ##
+# Task:                             ##
+######################################
+# Self assesment on program runtime ##
+import time                         ##
+start_time = time.time()            ##
+######################################
+
+
+
+
+
+# End runtime measure
+runtime = time.time() - start_time
+
+# Print output
+
+
+# Print runtime
+print('RunTime: {} seconds'.format(round(runtime,4)))
